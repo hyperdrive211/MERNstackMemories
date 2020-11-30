1c179041601077167306a7369f2d7e0a587309a4
Johnny <jmccauley117@gmail.com>
2020-11-30 22:39:27 +0000

    started some of the back and front end

---
+++ server/controllers/posts.js
@@ -0,0 +1,26 @@
+const PostMessage = require('../models/postMessage'); 
+
+
+module.exports = {
+    getPosts: async (req, res) => {
+      try {
+          const postMessages = await PostMessage.find(); 
+          conseol.log(postMessge)
+         res.status(200).json(postMessages); 
+      }catch(error){
+          console.log(error); 
+          res.status(404).json({message: error.message});
+      }
+    }, 
+    createPost = async (req, res) => {
+       const post = req.body; 
+       const newPost = new PostMessage(post); 
+
+       try{
+          await newPost.save(); 
+          res.status(201).json(newPost); 
+       }catch(error){
+           res.status(409).json({message: error.message});
+       }
+    }
+}
---
+++ server/index.js
@@ -0,0 +1,21 @@
+const express = require('express'); 
+const bodyParser = require('body-parser'); 
+const mongoose = require('mongoose'); 
+const cors = require('cors'); 
+
+
+const app = express(); 
+app.use(bodyParser.json({limit: "30mb", extended: true})); 
+app.use(bodyParser.urlencoded({limit: "30mb", extended: true})); 
+app.use(cors()); 
+app.use("/posts", require('./routes/posts'));
+
+const CONNECTION_URL = "mongodb+srv://jmccauley117:5Luci6us@cluster0.wwvu4.mongodb.net/Memories?retryWrites=true&w=majority"; 
+
+const PORT = process.env.PORT || 5000; 
+
+mongoose.connect(CONNECTION_URL, {useNewUrlParser: true, useUnifiedTopology: true})
+.then(() => app.listen(PORT, () => console.log(`Server running on port ${PORT}`)))
+.catch((err) => console.log("Connection :" + err.message)); 
+
+mongoose.set('useFindAndModify',false); 
